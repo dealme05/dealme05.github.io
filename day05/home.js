5fc1435ab3fba42d252bcec7ebcc6b1ef9b1acc8
Muhamad Aidil Bin Johari <muhamad_aidil@hotmail.com>
2018-12-01 16:21:45 +0800

    011218

---
+++ day05/home.js
@@ -0,0 +1,41 @@
+var my_name;    /*declare a variable*/
+var my_age;
+var my_birthday;
+
+my_name="stackup";  /*give value to the box, my_name, declared above*/
+my_age="45";
+my_birthday="05/09/1973";
+
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthday);
+
+var my_address="Paris Ris";
+var my_test; /* if not declared the type, then it is type as undefined*/
+
+console.log(my_address);
+console.log(my_test);
+
+console.log(typeof(my_test)); /* to check on the type of var eg number, string etc*/
+
+var firstName;
+var lastName;
+var space;
+
+firstName="Aidil";
+lastName="Johari";
+space=" "
+var fullName = firstName + space + lastName;
+
+console.log(fullName);
+
+var fullName1 = firstName + " " + lastName;
+
+console.log(fullName1);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
